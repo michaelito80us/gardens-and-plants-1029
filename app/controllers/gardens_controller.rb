552ae63b52827a67fdf4257354b8a1ff777c39b7
Michael Epelboim <sdrmike@outlook.com>
2022-11-18 18:05:26 +0800

    added plants create and destroy with html code

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,5 +1,5 @@
 class GardensController < ApplicationController
-  before_action :set_garden, only: %i[ show edit update destroy ]
+  before_action :set_garden, only: %i[show edit update destroy]
 
   # GET /gardens
   def index
@@ -8,6 +8,7 @@ class GardensController < ApplicationController
 
   # GET /gardens/1
   def show
+    @plant = Plant.new
   end
 
   # GET /gardens/new
@@ -16,15 +17,14 @@ class GardensController < ApplicationController
   end
 
   # GET /gardens/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /gardens
   def create
     @garden = Garden.new(garden_params)
 
     if @garden.save
-      redirect_to @garden, notice: "Garden was successfully created."
+      redirect_to @garden, notice: 'Garden was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +33,7 @@ class GardensController < ApplicationController
   # PATCH/PUT /gardens/1
   def update
     if @garden.update(garden_params)
-      redirect_to @garden, notice: "Garden was successfully updated."
+      redirect_to @garden, notice: 'Garden was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,17 +42,18 @@ class GardensController < ApplicationController
   # DELETE /gardens/1
   def destroy
     @garden.destroy
-    redirect_to gardens_url, notice: "Garden was successfully destroyed."
+    redirect_to gardens_url, notice: 'Garden was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_garden
-      @garden = Garden.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def garden_params
-      params.require(:garden).permit(:name, :banner_url)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_garden
+    @garden = Garden.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def garden_params
+    params.require(:garden).permit(:name, :banner_url)
+  end
 end
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,5 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :garden
-
   # plant.garden
+
+  validates :name, presence: true
+  validates :image_url, presence: true
 end
